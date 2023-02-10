55b29c67252b1e2ec6d36c5b5c3f97758107372c
Gavin Stark <gavin@gstark.com>
2023-02-09 22:08:36 -0500

    Stuff for render

--- config/puma.rb
+++ config/puma.rb
@@ -39,5 +39,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # preload_app!
 
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+preload_app!
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
